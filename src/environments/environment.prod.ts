80149417a43fe6c7596ad7aa369d53e52ac588b5
Angel_PC <araozangel842@gmail.com>
2023-12-27 21:42:02 +0200

    Set env vars to github repo

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  url: 'https://api.giphy.com/v1/gifs/search?api_key=yhll65LWhGVIyEOFi1xaRoopnpVdLraa&q=messi'
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  url: 'https://api.giphy.com/v1/gifs/search?',
+  apiKey: 'yhll65LWhGVIyEOFi1xaRoopnpVdLraa'
+}
+//earge
